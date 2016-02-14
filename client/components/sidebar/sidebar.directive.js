eb2f3f899ea7582bf964314a6712bf8f47e9cec8
ronatgithub <ronny.mombasa@gmail.com>
2016-02-14 10:45:15 +0100

    cleanup of deleted files

--- client/components/sidebar/sidebar.directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('meancouchApp')
-  .directive('sidebar', () => ({
-    templateUrl: 'components/sidebar/sidebar.html',
-    restrict: 'E',
-    controller: 'SidebarController',
-    controllerAs: 'sidebar'
-  }));
