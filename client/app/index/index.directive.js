45551968078beec1e304d1538002ce42317fee68
ronatgithub <ronny.mombasa@gmail.com>
2016-08-19 17:02:29 +0200

    frontpage responsive grid working.

--- client/app/index/index.directive.js
+++ client/app/index/index.directive.js
@@ -5,8 +5,45 @@ angular.module('meancouchApp')
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
-        angular.element(element).responsivegrid(scope.$eval(attrs.resgrid));
+        // angular.element(element).responsivegrid(scope.$eval(attrs.resgrid));
         // console.log(angular.element(element));
+
+        angular.element(element).responsivegrid({
+      gutter : '0',
+      itemSelector : '.grid-item',
+      'breakpoints': {
+        'desktop' : {
+          'range' : '1200-',
+          'options' : {
+            'column' : 20,
+          }
+        },
+        'tablet-landscape' : {
+          'range' : '1000-1200',
+          'options' : {
+            'column' : 20,
+          }
+        },
+        'tablet-portrate' : {
+          'range' : '767-1000',
+          'options' : {
+            'column' : 20,
+          }
+        },
+        'mobile-landscape' : {
+          'range' : '-767',
+          'options' : {
+            'column' : 10,
+          }
+        },
+        'mobile-portrate' : {
+          'range' : '-479',
+          'options' : {
+            'column' : 10,
+          }
+        },
+      }
+    });
 
         scope.$on("$destroy", function(){
           // Remove jQuery plugin from element to prevent memory leaks
