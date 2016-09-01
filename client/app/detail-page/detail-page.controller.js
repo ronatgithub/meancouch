3aa7c9948d6067270bb42e8784feceb3d775f0e8
ronatgithub <ronny.mombasa@gmail.com>
2016-09-01 20:27:57 +0200

    ui-bootstrap accordion fully working

--- client/app/detail-page/detail-page.controller.js
+++ client/app/detail-page/detail-page.controller.js
@@ -135,7 +135,81 @@ angular.module('meancouchApp')
       //  $document.on('scroll', function() {
       //    console.log('Document scrolled to ', $document.scrollLeft(), $document.scrollTop());
       //  });
+
+      // bootstrap collapse with angular ui.bootstrap (http://stackoverflow.com/questions/21436237/using-bootstrap-collapse-with-angularjs)
+        // show all at the same time or only one at a time
+        /*$scope.oneAtATime = true;
+
+        $scope.groups = [
+          {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+          },
+          {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+          }
+        ];
+
+        $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+        $scope.addItem = function() {
+          var newItemNo = $scope.items.length + 1;
+          $scope.items.push('Item ' + newItemNo);
+        };
+
+        $scope.status = {
+          isCustomHeaderOpen: false,
+          isFirstOpen: true,
+          isFirstDisabled: false
+        };*/
+
+        // add item html
+          // from http://jsfiddle.net/choroshin/U89bW/3/ and http://stackoverflow.com/questions/21436237/using-bootstrap-collapse-with-angularjs
+          /*<accordion close-others="oneAtATime">
+              <accordion-group heading="Dynamic Body Content">
+                <p>The body of the accordion group grows to fit the contents</p>
+                    <button class="btn btn-small" ng-click="addItem()">Add Item</button>
+                  <div ng-repeat="item in items">{{item}}</div>
+              </accordion-group>
+            </accordion>*/
+
+          // add item js
+          /*$scope.items = ['Camera 1', 'Camera 2', 'Camera 3'];
+
+          $scope.addItem = function() {
+            var newItemNo = $scope.items.length + 1;
+            $scope.items.push('Camera ' + newItemNo);
+          };*/
+      
   })
+
+.controller('AccordionDemoCtrl', function ($scope) {
+  $scope.oneAtATime = false;
+  $scope.groups = [
+    {
+      title: "Visit: Sipan/Trstenik - 1",
+      content: "Among going manor who did. Do ye is celebrated it sympathize considered. May ecstatic did surprise elegance the ignorant age. Own her miss cold last. It so numerous if he outlived disposal. How but sons mrs lady when. Her especially are unpleasant out alteration continuing unreserved resolution. Hence hopes noisy may china fully and. Am it regard stairs branch thirty length afford. - 1",
+      open: false
+    },
+    {
+      title: "Dynamic Group Header - 2",
+      content: "Dynamic Group Body - 2",
+      open: false
+    }
+  ];
+  
+  $scope.opened = function (group, i) {
+    console.log("Opened group with offset: "+ i);
+  };
+  $scope.$watch('groups', function(groups){
+    angular.forEach(groups, function(group, idx){
+      if (group.open) {
+        console.log("Opened group with idx: "+idx);
+      }
+    })   
+  }, true);
+})
 
   .controller('ModalBookingFormCtrl', function ($scope, $modal, $log) {
 
