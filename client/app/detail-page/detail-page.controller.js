1f499cbd9d7c10d94a5d576c3e051c1a26f190e0
ronatgithub <ronny.mombasa@gmail.com>
2016-09-02 11:03:44 +0200

    ui-bootstrap accordion fully working

--- client/app/detail-page/detail-page.controller.js
+++ client/app/detail-page/detail-page.controller.js
@@ -51,6 +51,32 @@ angular.module('meancouchApp')
 
       self.showDoc(id);
 
+      // ui-accordion true/false switches to change states
+      $scope.oneAtATime = true;
+      $scope.manyAtATime = false;
+      $scope.status = {
+        isCustomHeaderOpen: false,
+        isFirstOpen: false,
+        isFirstDisabled: false,
+        open: false
+      };
+
+      // ui-select options for month
+      self.monthArray = [
+            {id: 1, name: 'January'},
+            {id: 2, name: 'February'},
+            {id: 3, name: 'March'},
+            {id: 4, name: 'April'},
+            {id: 5, name: 'May'},
+            {id: 6, name: 'June'},
+            {id: 7, name: 'July'},
+            {id: 8, name: 'August'},
+            {id: 9, name: 'September'},
+            {id: 10, name: 'October'},
+            {id: 11, name: 'November'},
+            {id: 12, name: 'December'},
+        ];
+
       // angular-slick-carousel
       $scope.slickConfig1 = {
           slidesToShow: 1,
@@ -185,7 +211,6 @@ angular.module('meancouchApp')
   })
 
   .controller('AccordionCtrl', function ($scope) {
-    $scope.oneAtATime = false;
     $scope.groups = [
       {
         title: "Visit: Tsavo East",
